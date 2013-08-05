f425d35af4d8ad89aa8e179b14575d6393da8dd2
Dave Umrysh <dave@umrysh.com>
2013-08-04 22:08:03 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Piwigo Twitter Cards
+==============
+
+This is a [Piwigo](http://piwigo.org/) plugin that will allow you to enable [Twitter Photo Cards](https://dev.twitter.com/docs/cards/types/photo-card) when you tweet a link to your photos.
+
+NOTE:
+* Before the photo cards will be enabled on Twitter you must submit your site with the [Twitter Cards Validator](https://dev.twitter.com/docs/cards/validation/validator).
+
+Enjoy!
+
+
+
+
+Contributing
+------------
+
+Feel free to fork and send [pull requests](http://help.github.com/fork-a-repo/).  Contributions welcome.
+
+Credit
+------------
+
+The code to create the thumbnails was provided by [mthorn.net](http://mthorn.net/)
+
+License
+-------
+
+This script is open source software released under the GNU GENERAL PUBLIC LICENSE V3.
---
+++ main.inc.php
@@ -0,0 +1,150 @@
+<?php
+/*
+Version: 1.0
+Plugin Name: TwitterCards
+Author: umrysh
+Description: Twitter Cards
+*/
+
+// Check whether we are indeed included by Piwigo.
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+ 
+class TwitterCard
+{
+  function twittercard_load ($content)
+  {
+    global $template,$picture,$page;
+    
+    // get image url
+    $query = sprintf('
+		  select path, name
+		  FROM piwigo_images
+		  WHERE id = %s
+		;',
+		$page['image_id']);
+	$result = pwg_query($query);
+	$row = pwg_db_fetch_assoc($result);
+	$url = substr($row['path'],2);
+    $title= str_replace('"', '\"',$row['name']);
+
+    $extension_pos = strrpos($url, '.');
+    $thumb = substr($url, 0, $extension_pos) . '_tw_thumb' . substr($url, $extension_pos);
+    // Check if a thumb already exists, otherwise create a thumb
+    if (!file_exists( $thumb ))
+    {
+        /**
+        * Create a thumbnail image from $inputFileName no taller or wider than 
+        * $maxSize. Returns the new image resource or false on error.
+        * Author: mthorn.net
+        */
+        function thumbnail($inputFileName, $maxSize = 100)
+        {
+            $info = getimagesize($inputFileName);
+
+            $type = isset($info['type']) ? $info['type'] : $info[2];
+
+            // Check support of file type
+            if ( !(imagetypes() & $type) )
+            {
+                // Server does not support file type
+                return false;
+            }
+
+            $width  = isset($info['width'])  ? $info['width']  : $info[0];
+            $height = isset($info['height']) ? $info['height'] : $info[1];
+
+            // Calculate aspect ratio
+            $wRatio = $maxSize / $width;
+            $hRatio = $maxSize / $height;
+
+            // Using imagecreatefromstring will automatically detect the file type
+            $sourceImage = imagecreatefromstring(file_get_contents($inputFileName));
+
+            // Calculate a proportional width and height no larger than the max size.
+            if ( ($width <= $maxSize) && ($height <= $maxSize) )
+            {
+                // Input is smaller than thumbnail, do nothing
+                return $sourceImage;
+            }
+            elseif ( ($wRatio * $height) < $maxSize )
+            {
+                // Image is horizontal
+                $tHeight = ceil($wRatio * $height);
+                $tWidth  = $maxSize;
+            }
+            else
+            {
+                // Image is vertical
+                $tWidth  = ceil($hRatio * $width);
+                $tHeight = $maxSize;
+            }
+
+            $thumb = imagecreatetruecolor($tWidth, $tHeight);
+
+            if ( $sourceImage === false )
+            {
+                // Could not load image
+                return false;
+            }
+
+            // Copy resampled makes a smooth thumbnail
+            imagecopyresampled($thumb, $sourceImage, 0, 0, 0, 0, $tWidth, $tHeight, $width, $height);
+            imagedestroy($sourceImage);
+
+            return $thumb;
+        }
+
+        /**
+         * Save the image to a file. Type is determined from the extension.
+         * $quality is only used for jpegs.
+         * Author: mthorn.net
+         */
+        function imageToFile($im, $fileName, $quality = 80)
+        {
+            if ( !$im || file_exists($fileName) )
+            {
+               return false;
+            }
+
+            $ext = strtolower(substr($fileName, strrpos($fileName, '.')));
+
+            switch ( $ext )
+            {
+                case '.gif':
+                    imagegif($im, $fileName);
+                    break;
+                case '.jpg':
+                case '.jpeg':
+                    imagejpeg($im, $fileName, $quality);
+                    break;
+                case '.png':
+                    imagepng($im, $fileName);
+                    break;
+                case '.bmp':
+                    imagewbmp($im, $fileName);
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        $im = thumbnail($url, 435);
+        imageToFile($im, $thumb);
+    }
+
+    $info = getimagesize( $thumb );
+    $width  = isset($info['width'])  ? $info['width']  : $info[0];
+    $height = isset($info['height']) ? $info['height'] : $info[1];
+    
+
+    $template->append('head_elements',
+    '<meta name="twitter:card" content="photo"><meta name="twitter:title" content="' . $title . '"><meta name="twitter:image" content="http://' . $_SERVER['HTTP_HOST'] . '/' . $thumb . '"><meta name="twitter:image:width" content="' . $width . '"><meta name="twitter:image:height" content="' . $height . '"><meta property="og:title" content="' . $title . '" /><meta property="og:image" content="http://' . $_SERVER['HTTP_HOST'] . '/' . $thumb . '" />');
+  }
+}
+
+$obj = new Twittercard();
+add_event_handler('render_element_content', array(&$obj, 'twittercard_load'));
+
+?>
