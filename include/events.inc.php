aa7f4ba6375e7803ddfebecfaa01658af79b57c5
David <me@daviddernoncourt.com>
2016-06-19 17:38:21 +0200

    Add Open Graph description

--- include/events.inc.php
+++ include/events.inc.php
@@ -150,6 +150,7 @@ class TwitterCard
     <meta name="twitter:site" content="' . $twitter_site . '">
     <meta name="twitter:image" content="http://' . $_SERVER['HTTP_HOST'] . substr($_SERVER['REQUEST_URI'], 0, strrpos(substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '?')), '/')) . '/' . $thumbLocal . '">
 	<meta property="og:title" content="'.$title.'"/>
+    <meta property="og:description" content="' . $description . '"/>
 	<meta property="og:type" content="website"/>
 	<meta property="og:url" content="https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] .'"/>
 	<meta property="og:image" content="https://' . $_SERVER['HTTP_HOST'] . substr($_SERVER['REQUEST_URI'], 0, strrpos(substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '?')), '/')) . '/' . $url . '"/>
